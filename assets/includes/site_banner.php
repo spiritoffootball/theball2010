a2b955a9b4ca03d43d40388f23ef352db1596dea
Christian Wach <needle@haystack.co.uk>
2018-04-30 13:13:53 +0100

    codestyle

--- assets/includes/site_banner.php
+++ assets/includes/site_banner.php
@@ -1,38 +1,43 @@
-<!-- site_banner.php -->
+<?php /*
+================================================================================
+Site Banner Template
+================================================================================
+AUTHOR: Christian Wach <needle@haystack.co.uk>
+--------------------------------------------------------------------------------
+NOTES
+
+--------------------------------------------------------------------------------
+*/
+
+?><!-- assets/includes/site_banner.php -->
 
 <div id="site_banner" class="clearfix">
-<div id="site_banner_inner">
 
+	<div id="site_banner_inner">
 
+		<?php include( get_template_directory() . '/assets/includes/network.php' ); ?>
 
-<?php include( get_template_directory() . '/assets/includes/network.php' ); ?>
+		<div id="splash">
 
+			<a href="/2010/files/2009/11/ak_children_with_ball_640.jpg"><img src="/2010/files/2009/11/ak_children_with_ball_640-320x240.jpg" alt="Children with an Alive &amp; Kicking ball" title="Children with an Alive &amp; Kicking ball" width="200" height="150" class="size-medium wp-image-52" /></a>
 
+		</div><!-- /splash -->
 
-<div id="splash">
+		<div id="banner_copy">
 
-<a href="/2010/files/2009/11/ak_children_with_ball_640.jpg"><img src="/2010/files/2009/11/ak_children_with_ball_640-320x240.jpg" alt="Children with an Alive &amp; Kicking ball" title="Children with an Alive &amp; Kicking ball" width="200" height="150" class="size-medium wp-image-52" /></a>
+			<h2>Welcome to <?php bloginfo( 'title' ); if ( is_home() ) { echo ' blog'; } ?></h2>
 
-</div><!-- /splash -->
+			<p><em>"An African ball made for the poorest African communities &mdash; the very embodiment of development through football in Africa"</em><br />
+			&mdash; James Flecker of Alive &amp; Kicking</p>
 
+			<?php if ( ! is_home() ) { ?>
+				<p id="gotoblog"><a href="/2010/blog/">Read the blog &rarr;</a></p>
+			<?php } ?>
 
+		</div><!-- /banner_copy -->
 
-<div id="banner_copy">
+	</div><!-- /site_banner_inner -->
 
-<h2>Welcome to <?php bloginfo( 'title' ); if ( is_home() ) { echo ' blog'; } ?></h2>
-
-<p><em>"An African ball made for the poorest African communities &mdash; the very embodiment of development through football in Africa"</em><br />
-&mdash; James Flecker of Alive &amp; Kicking</p>
-
-<?php if ( ! is_home() ) { ?>
-	<p id="gotoblog"><a href="/2010/blog/">Read the blog &rarr;</a></p>
-<?php } ?>
-
-</div><!-- /banner_copy -->
-
-
-
-</div><!-- /site_banner_inner -->
 </div><!-- /site_banner -->
 
 
--- assets/includes/supporters_2010.php
+++ assets/includes/supporters_2010.php
@@ -1,31 +1,42 @@
-<!-- Begin sponsors -->
+<?php /*
+================================================================================
+Supporters 2010 Template
+================================================================================
+AUTHOR: Christian Wach <needle@haystack.co.uk>
+--------------------------------------------------------------------------------
+NOTES
+
+--------------------------------------------------------------------------------
+*/
+
+?><!-- assets/includes/supporters_2010.php -->
 <div id="sponsors" class="clearfix">
 
-<div id="sponsor_logos">
+	<div id="sponsor_logos">
 
-<ul>
+		<ul>
 
-<li><a href="http://specialolympicsafrica.co.za/" title="Special Olympics"><img class="partner_logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/partners/soa_logo.png" alt="Special Olympics Logo" /></a></li>
+			<li><a href="http://specialolympicsafrica.co.za/" title="Special Olympics"><img class="partner_logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/partners/soa_logo.png" alt="Special Olympics Logo" /></a></li>
 
-<li><a href="http://aliveandkicking.org.uk/" title="Alive &amp; Kicking"><img class="partner_logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/partners/ak_ball_logo.png" alt="Alive &amp; Kicking Logo" /></a></li>
+			<li><a href="http://aliveandkicking.org.uk/" title="Alive &amp; Kicking"><img class="partner_logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/partners/ak_ball_logo.png" alt="Alive &amp; Kicking Logo" /></a></li>
 
-<li><a href="http://www.dhl.co.za/publish/za/en.high.html" title="DHL Express Sub-Saharan Africa"><img class="partner_logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/partners/dhl_logo.png" alt="DHL Express Sub-Saharan Africa Logo" /></a></li>
+			<li><a href="http://www.dhl.co.za/publish/za/en.high.html" title="DHL Express Sub-Saharan Africa"><img class="partner_logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/partners/dhl_logo.png" alt="DHL Express Sub-Saharan Africa Logo" /></a></li>
 
-<li><a href="http://www.goethe.de/todo-aleman" title="Todo Aleman"><img class="partner_logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/partners/todo_aleman_logo.jpg" alt="Todo Aleman Logo" /></a></li>
+			<li><a href="http://www.goethe.de/todo-aleman" title="Todo Aleman"><img class="partner_logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/partners/todo_aleman_logo.jpg" alt="Todo Aleman Logo" /></a></li>
 
-<li><a href="http://spiritoffootball.com/" title="Spirit of Football CIC"><img class="partner_logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/partners/sof_logo.png" alt="Spirit of Football CIC Logo" /></a></li>
+			<li><a href="http://spiritoffootball.com/" title="Spirit of Football CIC"><img class="partner_logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/partners/sof_logo.png" alt="Spirit of Football CIC Logo" /></a></li>
 
-<li><a href="http://www.spirit-of-football.de/" title="Spirit of Football e.V."><img class="partner_logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/partners/sof_ev_logo.png" alt="Spirit of Football e.V. Logo" /></a></li>
+			<li><a href="http://www.spirit-of-football.de/" title="Spirit of Football e.V."><img class="partner_logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/partners/sof_ev_logo.png" alt="Spirit of Football e.V. Logo" /></a></li>
 
-<li><a href="http://www.africa10.com/" title="Africa 10"><img class="partner_logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/partners/africa10_logo.png" alt="Africa 10 Logo" /></a></li>
+			<li><a href="http://www.africa10.com/" title="Africa 10"><img class="partner_logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/partners/africa10_logo.png" alt="Africa 10 Logo" /></a></li>
 
-<li><a href="http://freestylefootball.org/" title="Freestyle Football Federation"><img class="partner_logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/partners/f3_logo.png" alt="Freestyle Football Federation Logo" /></a></li>
+			<li><a href="http://freestylefootball.org/" title="Freestyle Football Federation"><img class="partner_logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/partners/f3_logo.png" alt="Freestyle Football Federation Logo" /></a></li>
 
-<li><a href="http://www.gamechangers.com/" title="GameChangers"><img class="partner_logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/partners/gamechangers_logo.png" alt="GameChangers Logo" /></a></li>
+			<li><a href="http://www.gamechangers.com/" title="GameChangers"><img class="partner_logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/partners/gamechangers_logo.png" alt="GameChangers Logo" /></a></li>
 
-</ul>
+		</ul>
 
-</div><!-- sponsor_logos -->
+	</div><!-- sponsor_logos -->
 
 </div><!-- /sponsors -->
 
